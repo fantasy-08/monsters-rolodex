b4ab73d57a7257cfa7613e99f8bcfedaa9a19afd
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-10-07 16:49:47 +0530

    Updates

---
+++ precache-manifest.8d0779c09e66ba04c92d9e7532fa6ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810e14f112e37b98281d784ef06f8e73",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e31c7ac61a881614aaa",
+    "url": "/monsters-rolodex/static/css/main.1c7a4c21.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e31c7ac61a881614aaa",
+    "url": "/monsters-rolodex/static/js/main.16e5755a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
